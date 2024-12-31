8b5f2025955907bcae39e29ae5598373467a118e
Roberto Rodriguez <9653181+Cyb3rWard0g@users.noreply.github.com>
2024-12-31 17:39:34 -0500

    Updating version to 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="floki-ai",
-    version="0.6.0",
+    version="0.7.0",
     author="Roberto Rodriguez",
     description="Agentic Workflows Made Simple",
     long_description=long_description,
