a3b20aedf41fa55a3db3a104f13ee75cb152be7b
Roberto Rodriguez <9653181+Cyb3rWard0g@users.noreply.github.com>
2025-01-07 12:42:01 -0500

    Added basic OpenAI Embeddings client and created / updated notebooks

--- src/floki/__init__.py
+++ src/floki/__init__.py
@@ -3,7 +3,7 @@ from floki.agent import (
     AgenticWorkflowService, RoundRobinWorkflowService, RandomWorkflowService,
     LLMWorkflowService, ReActAgent, ToolCallAgent, OpenAPIReActAgent
 )
-from floki.llm.openai import OpenAIChatClient, OpenAIAudioClient
+from floki.llm.openai import OpenAIChatClient, OpenAIAudioClient, OpenAIEmbeddingClient
 from floki.llm.huggingface import HFHubChatClient
 from floki.tool import AgentTool, tool
 from floki.workflow import WorkflowApp
